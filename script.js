660291169448924a60c81b3bdbb92e16357ae98d
atiks88 <141978852+atiks88@users.noreply.github.com>
2023-10-31 14:16:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+  const container = document.querySelector('.container');
+const panels = document.querySelectorAll('.panel');
+
+container.addEventListener('click', (event) => {
+  const clickedPanel = event.target;
+  
+  // Check if the clicked element is a panel
+  if (clickedPanel.classList.contains('panel')) {
+    // Remove the "active" class from all panels
+    panels.forEach(panel => {
+      panel.classList.remove('active');
+    });
+
+    // Add the "active" class to the clicked panel
+    clickedPanel.classList.add('active');
+  }
+});
